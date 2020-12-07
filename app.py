ac82614d64592437c387f9c666bd5880c46ee70f
arun-ajay <arunajay42795@gmail.com>
2020-12-06 22:35:25 -0500

    envirment variables setup + secret_hash helper function

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from flask import Flask, request, jsonify
 import boto3
 import os
+import botocore.exceptions
+import botocore.errorfactory
+import hmac
+import hashlib
+import base64
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 application = app
-
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+USER_POOL_ID = os.environ.get("USER_POOL_ID")
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
 #----------------------------HELPER FUNCTIONS----------------------------#
 def authorizeuser():
     pass
+
+def get_secret_hash(username,CLIENT_ID,CLIENT_SECRET):
+    msg = username + CLIENT_ID
+    dig = hmac.new(str(CLIENT_SECRET).encode('utf-8'), msg = str(msg).encode('utf-8'), digestmod=hashlib.sha256).digest()
+    d2 = base64.b64encode(dig).decode()
+    return d2
 
 #Cognito
 @app.route('/signup', methods = ["POST"])
@@ -34,7 +52,7 @@ def login():
 
 #Cognito + DynamoDB
 @app.route('/getuser', methods = ["POST"])
-def signup():
+def getuser():
     pass
 
 #Cognito
@@ -48,3 +66,7 @@ def confirmforgotpassword():
     pass
 
 
+if __name__ == '__main__':
+
+
+    app.run()
